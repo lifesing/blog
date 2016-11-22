4837d37a7f6cf03de19f4e13f8aa231ecedeace5
王涛 <wangtao@iscs.com.cn>
2016-11-22 19:56:32 +0800

    user edit

--- src/admin/controller/api/user.js
+++ src/admin/controller/api/user.js
@@ -3,7 +3,7 @@
  * @Descrition: 用户模块 rest  api
  * @Created time: 2016-11-10 14:10:19 
  * @Last Modified by: anytao
- * @Last Modified time: 2016-11-10 16:03:51
+ * @Last Modified time: 2016-11-22 19:09:26
  */
 
 'use strict';
@@ -14,16 +14,43 @@ export default class extends Base {
 
 
     /**
+     * 用户列表
+     */
+    getAction() {
+
+        let where = {};
+        if (this.id) {
+            where.id = this.id;
+        }
+        this.modelInstance.field('id,username,nickname,email,type,status,created_at').where(where);
+        return super.getAction();
+    }
+
+
+    /**
      *  add user
      */
     async postAction() {
 
+        //返回对象
+        let response = {
+            success: true,
+            data: '',
+            msg: ''
+        };
+        let result = {};
         let data = this.post();
-        let userID = await this.modelInstance.addUser(data, this.ip());
-        return this.success({
-            id: userID
-        });
-
+        data.status = 1;
+        data.type = 1;
+        result = await this.modelInstance.addUser(data, this.ip());
+        if (result.type == 'add') {
+            response.msg = '新增成功';
+            response.data = result.id;
+        } else if (result.type == 'exist') {
+            response.success = false;
+            response.msg = '用户已存在';
+        }
+        return this.json(response);
     }
 
 }
